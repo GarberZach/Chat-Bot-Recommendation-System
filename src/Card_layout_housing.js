021d3a117b7ed5f588ccfc8d955e6ee11f587381
GarberZach <zachgarber2436@gmail.com>
2023-07-25 13:45:08 -0400

    housing

---
+++ src/Card_layout_housing.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./card_styles.css"
+function Card_layout_housing(recs) {
+    
+var data = recs.recs;
+console.log(data);
+if(recs.recs.length == 0) return <div class="table-wrapper"><p>No Recommendations</p></div>
+  return(
+   
+        <div class="container">
+            <div class="carousel">
+            { 
+                    data.map((item, index) => (
+                        
+                            <input type="radio" name="slides" id={"slide-" + index}></input>
+                        
+                    )       
+
+                    )
+                } 
+                <ul class="carousel__slides">
+
+
+
+                {
+                        
+                    data.map((item) => (
+                    
+                    <li class="carousel__slide">
+                        <figure>
+                            <div>
+                                <img src={item.cover_url} alt=""></img>
+                            </div>
+                            <figcaption>
+                                {item.name}
+                                {item.artist}
+                                {item.album}
+                                
+                            </figcaption>
+                        </figure>
+                    </li>
+                    ))
+                }
+                </ul>
+                <ul class="carousel__thumbnails">
+
+                {   
+                        data.map((item, index) => (
+                        <li>
+                            <label for={"slide-" + index}><img src={item.cover_url} alt=""></img></label>
+                        </li>
+                        ))
+                    }    
+                </ul>
+            </div>
+        </div>
+    
+  )
+}
+
+export default Card_layout_housing;
